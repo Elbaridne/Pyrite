c23398efdd67b53ec2aea8827c2f97e9d09401f7
Mario <mariobluemind@gmail.com>
2018-03-22 20:18:51 +0100

    echo: Mocking you replacing vocals with an i and randomly capitalizing characters

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import re
+import random
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -26,11 +28,30 @@ class Pybot:
     def help(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Jeje ni puta idea ")
 
+    def echo(bot, update):
+        mens = str(update.message.text)
+        ii = re.sub("[aAoOuU]", "i", mens)
+        stri = ''
+        for num in ii:
+            ran = random.randint(0,1)
+            if(ran == 1):
+                if(num == 'i'):
+                    stri = stri + num
+                    continue
+                stri = stri + num.capitalize()
+
+            else:
+                stri = stri + num.lower()
+        print(str(ii))
+        bot.send_message(chat_id=update.message.chat_id, text=stri)
+
     def handlers(self):
         start_handler = CommandHandler('start', Pybot.start)
         help_handler = CommandHandler('help', Pybot.help)
+        echo_handler = MessageHandler(Filters.text, Pybot.echo)
         Pybot.dispatcher.add_handler(start_handler)
         Pybot.dispatcher.add_handler(help_handler)
+        Pybot.dispatcher.add_handler(echo_handler)
 
 
 
